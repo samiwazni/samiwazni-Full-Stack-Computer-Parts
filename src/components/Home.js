54763bc523adeda8181e74967d1882eb50809650
Sami Wazni <63284324+samiwazni@users.noreply.github.com>
2024-09-09 23:27:41 +0300

    Updated the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Container, Button, Row, Col } from "react-bootstrap";
+import "../style/style.scss";
+import Hero from "../assets/hero.png"
 
 const Home = ({ currentUser }) => {
   return (
-    <div className="Home-Container">
-      <h1>Welcome to the Pc Building Website.</h1>
-      <p>
-        Exploring a React.js Computer Configurator: Empowering Users to
-        Customize PC Builds with Data Scraping and Database Integration. This
-        project focuses on the development process of a custom computer
-        configurator, a user-friendly web-based application that empowers users
-        to create personalized computer systems. The project is composed of
-        three core components: data scraping, dynamic web design, and database
-        integration.
-      </p>
-      {!currentUser && (
-        <p>
-          To use the <strong>Computer Builder</strong> please{" "}
-          <Link to="/signin">sign in</Link>
-        </p>
-      )}
-      <br></br>
-      <a variant="contained" aria-label="outlined primary button group">
-        <button>
-          <Link to="components">Components</Link>
-        </button>
-      </a>
+    <div className="home-bg">
+      <Container className="text-container">
+        <Row className="align-items-center">
+          <Col md={6}>
+            <div className="text-white pt-5 pb-5 rounded-lg">
+              <h1 className="display-6">Welcome to the PC Building Website</h1>
+              <p className="lead mt-4">
+                Take a look at the latest computer parts that our database has.
+              </p>
+              <p className="mt-4">
+                Our project is a full-stack application built with:
+              </p>
+              <ul className="lead">
+                <li>ReactJS for the front end</li>
+                <li>NodeJS for the back end</li>
+                <li>Python for creating the database tables</li>
+                <li>SQLite for the database</li>
+              </ul>
+              {!currentUser && (
+                <p className="mt-4">
+                  To use the <strong>Computer Builder</strong>{" "}
+                  <Button variant="outline-info" size="sm">
+                    <Link to="/signin" className="text-info text-decoration-none">
+                      sign in
+                    </Link>
+                </Button>
+                  
+                </p>
+              )}
+              <Button variant="dark" size="sm" className="mt-3">
+                <Link to="/components" className="text-white text-decoration-none">
+                  Components
+                </Link>
+              </Button>
+            </div>
+          </Col>
+          <Col md={6}>
+            <div className="image-container">
+              <img
+                src={Hero}
+                alt="Motherboard"
+                className="img-fluid"
+              />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
